95edae139c67af746ae4c57b515980bd404ab51e
bommox@gmail.com <Jorge Blom-Dahl>
2017-11-17 10:03:23 +0000

    Responder al get

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-import SimpleHTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SocketServer
 
-PORT = 8000
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    def do_GET(self):
+            self._set_headers()
+            self.wfile.write("<html><body><h1>hi!</h1></body></html>")
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+def run(server_class=HTTPServer, handler_class=S, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print 'Starting httpd...'
+    httpd.serve_forever()
 
-print "serving at port", PORT
-httpd.serve_forever()
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
 
